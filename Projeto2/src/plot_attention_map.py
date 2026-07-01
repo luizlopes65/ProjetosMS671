08b731e450ccd70287dfbc5174bf8ca3fabf3a7d
monica-2605 <monicahenriques2017@gmail.com>
2026-06-30 21:47:00 -0300

    Update plot_attention_map.py

--- Projeto2/src/plot_attention_map.py
+++ Projeto2/src/plot_attention_map.py
@@ -1,94 +1,103 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import to_categorical
 
-def plot_attention_map(modelx, input_vocabulary, inv_output_vocabulary, text, n_s = 128, num = 7):
+# Funções auxiliares para mapear texto para inteiro e vice-versa
+def _string_to_int(string, length, vocabulary):
+    string = string.lower()
+    string = string.replace(',','')
+    # Se o caractere não existir no vocabulário, usa um valor padrão ou ignora
+    rep = [vocabulary.get(char, 0) for char in string]
+    if len(rep) < length:
+        rep += [vocabulary.get('<pad>', 0)] * (length - len(rep))
+    elif len(rep) > length:
+        rep = rep[:length]
+    return rep
+
+def _int_to_string(ints, inv_vocabulary):
+    return [inv_vocabulary.get(idx, '<unk>') for idx in ints]
+
+def plot_attention_map(modelx, input_vocabulary, output_vocabulary, text, n_s = 64, num = 7):
     """
     Plot the attention map.
-
     """
     attention_map = np.zeros((10, 30))
-    layer = modelx.get_layer('attention_weights')
-
     Ty, Tx = attention_map.shape
 
-    human_vocab_size = 37
-
-    # Well, this is cumbersome but this version of tensorflow-keras has a bug that affects the
-    # reuse of layers in a model with the functional API.
-    # So, I have to recreate the model based on the functional
-    # components and connect then one by one.
-    # ideally it can be done simply like this:
-    # layer = modelx.layers[num]
-    # f = Model(modelx.inputs, [layer.get_output_at(t) for t in range(Ty)])
-    #
-
+    # 1. Obter as entradas originais do modelo
     X = modelx.inputs[0]
     s0 = modelx.inputs[1]
     c0 = modelx.inputs[2]
     s = s0
-    c = s0
+    c = c0
 
     a = modelx.layers[2](X)
     outputs = []
 
+    # Reconstruindo o fluxo para capturar os pesos de atenção (alphas)
     for t in range(Ty):
         s_prev = s
         s_prev = modelx.layers[3](s_prev)
         concat = modelx.layers[4]([a, s_prev])
         e = modelx.layers[5](concat)
         energies = modelx.layers[6](e)
-        alphas = modelx.layers[7](energies)
+        alphas = modelx.layers[7](energies) # Camada Softmax da Atenção
         context = modelx.layers[8]([alphas, a])
-        # Don't forget to pass: initial_state = [hidden state, cell state] (≈ 1 line)
         s, _, c = modelx.layers[10](context, initial_state = [s, c])
-        outputs.append(energies)
+        
+        # Guardamos 'alphas' (pesos de atenção) em vez de 'energies' para o mapa fazer sentido
+        outputs.append(alphas)
 
-    f = modelx(inputs=[X, s0, c0], outputs = outputs)
+    # CORREÇÃO DO ERRO: Usar a classe Model importada do Keras
+    f = Model(inputs=[X, s0, c0], outputs=outputs)
 
-
-    s0 = np.zeros((1, n_s))
-    c0 = np.zeros((1, n_s))
-    encoded = np.array(string_to_int(text, Tx, input_vocabulary)).reshape((1, 30))
+    # 2. Preparação dos dados de entrada
+    s0_val = np.zeros((1, n_s))
+    c0_val = np.zeros((1, n_s))
+    
+    encoded = np.array(_string_to_int(text, Tx, input_vocabulary)).reshape((1, 30))
     encoded = np.array(list(map(lambda x: to_categorical(x, num_classes=len(input_vocabulary)), encoded)))
 
-
-    r = f([encoded, s0, c0])
+    # Fazer a predição dos pesos de atenção
+    r = f([encoded, s0_val, c0_val])
 
     for t in range(Ty):
         for t_prime in range(Tx):
-            attention_map[t][t_prime] = r[t][0, t_prime]
+            # r[t] tem formato (batch_size, Tx, 1) -> tiramos o squeeze para mapear
+            attention_map[t][t_prime] = np.squeeze(r[t])[t_prime]
 
-    # Normalize attention map
+    # Normalizar o mapa de atenção
     row_max = attention_map.max(axis=1)
+    # Evita divisão por zero caso haja alguma linha zerada
+    row_max[row_max == 0] = 1.0
     attention_map = attention_map / row_max[:, None]
 
-    prediction = modelx.predict([encoded, s0, c0])
-
+    # Prever o texto final para colocar nos eixos do gráfico
+    prediction = modelx.predict([encoded, s0_val, c0_val], verbose=0)
+    
     predicted_text = []
     for i in range(len(prediction)):
-        predicted_text.append(int(np.argmax(prediction[i], axis=1)))
+        predicted_text.append(int(np.argmax(prediction[i], axis=1)[0]))
 
-    predicted_text = list(predicted_text)
-    predicted_text = int_to_string(predicted_text, inv_output_vocabulary)
+    inv_output_vocabulary = {v: k for k, v in output_vocabulary.items()}
+    predicted_text = _int_to_string(predicted_text, inv_output_vocabulary)
     text_ = list(text)
 
-    # get the lengths of the string
     input_length = len(text)
     output_length = Ty
 
-    # Plot the attention_map
+    # 3. Plotagem do Gráfico usando Matplotlib
     plt.clf()
-    f = plt.figure(figsize=(8, 8.5))
-    ax = f.add_subplot(1, 1, 1)
+    fig = plt.figure(figsize=(8, 8.5))
+    ax = fig.add_subplot(1, 1, 1)
 
-    # add image
-    i = ax.imshow(attention_map, interpolation='nearest', cmap='Blues')
+    i = ax.imshow(attention_map[:, :input_length], interpolation='nearest', cmap='Blues')
 
-    # add colorbar
-    cbaxes = f.add_axes([0.2, 0, 0.6, 0.03])
-    cbar = f.colorbar(i, cax=cbaxes, orientation='horizontal')
+    cbaxes = fig.add_axes([0.2, 0.03, 0.6, 0.03])
+    cbar = fig.colorbar(i, cax=cbaxes, orientation='horizontal')
     cbar.ax.set_xlabel('Alpha value (Probability output of the "softmax")', labelpad=2)
 
-    # add labels
     ax.set_yticks(range(output_length))
     ax.set_yticklabels(predicted_text[:output_length])
 
@@ -97,10 +106,11 @@ def plot_attention_map(modelx, input_vocabulary, inv_output_vocabulary, text, n_
 
     ax.set_xlabel('Input Sequence')
     ax.set_ylabel('Output Sequence')
+    ax.grid(False) # Desliga linhas de grade sobrepostas ao mapa
 
-    # add grid and legend
-    ax.grid()
-
-    #f.show()
+    # Salva ou exibe o gráfico
+    plt.savefig('attention_map.png', bbox_inches='tight')
+    print("Mapa de atenção gerado com sucesso e salvo como 'attention_map.png'!")
+    plt.close()
 
     return attention_map
